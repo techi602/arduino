23246c8306c1930609f907e1df6119a323fb32f6
Michal Vrchota <michal.vrchota@gmail.com>
2019-05-07 23:44:54 +0200

    T100 tweaking & oled support

--- T100/T100.ino
+++ T100/T100.ino
@@ -1,9 +1,36 @@
+/*
+ * http://home.kendra.com/mauser/Joystick.html
+ * http://luser.github.io/gamepadtest/
+ * http://internetexplorer.github.io/Gamepad-Sample/
+ */
+
+#ifdef ESP32
+
+#define PWMRANGE 1023
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiMulti.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
+#else
+
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-#include <WebSocketsServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+
+#endif
+
+#include <Arduino.h>
+#include <WebSocketsServer.h>
 #include <string.h>
+#include <Wire.h>
+#include <ACROBOTIC_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 
 // WiFi parameters
@@ -13,18 +40,24 @@
 #define WIFI_SSID "wifi"
 #endif
 
-#ifndef WIFI_PASSWORD 
+#ifndef WIFI_PASSWORD
 #define WIFI_PASSWORD ""
 #endif
-
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
+#ifdef ESP32
+//MDNSResponder mdns;
+WiFiMulti WiFiMulti;
+WebServer server(80);
+WebSocketsServer webSocket = WebSocketsServer(81);
+#else
 MDNSResponder mdns;
 ESP8266WiFiMulti WiFiMulti;
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
+#endif
 
 #define PWMA D6
 #define PWMB D4
@@ -43,15 +76,8 @@ int xspeed = PWMRANGE;
 int motor_speed_left = 0;
 int motor_speed_right = 0;
 
-static const char PROGMEM INDEX_HTML[] = R"rawliteral(
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <title>T100 control center</title>
-    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
-    <script rel="virtualjoystick.js">
-        var VirtualJoystick = function (opts) {
+static const char PROGMEM JOYSTICK_JS[] = R"rawliteral(
+var VirtualJoystick = function (opts) {
             opts = opts || {};
             this._container = opts.container || document.body;
             this._strokeStyle = opts.strokeStyle || 'cyan';
@@ -462,7 +488,16 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
             return exports;
         }
 
-    </script>
+)rawliteral";
+
+static const char PROGMEM INDEX_HTML[] = R"HTML(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <title>T100 control center</title>
+    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+    <script src="/virtualjoystick.js"></script>
     <script>
 
         var joystickX = 0;
@@ -477,6 +512,11 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
         var debugText;
 
         var powerOn = false;
+
+        document.addEventListener('mouseup', function () {
+          joystickMove(0,0);
+        });
+
 
         document.addEventListener('DOMContentLoaded', function () {
 
@@ -631,184 +671,255 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(
 </html>
 
 
-)rawliteral";
+)HTML";
 
-
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
 {
-  Serial.printf("webSocketEvent(%d, %d, ...)\r\n", num, type);
-  switch(type) {
+    Serial.printf("webSocketEvent(%d, %d, ...)\r\n", num, type);
+    switch (type)
+    {
     case WStype_DISCONNECTED:
-      Serial.printf("[%u] Disconnected!\r\n", num);
-      break;
+        Serial.printf("[%u] Disconnected!\r\n", num);
+        oled.clearDisplay();
+        oled.putString("Client disconnected");
+        break;
     case WStype_CONNECTED:
-      {
+    {
+      char buffer[40];
         IPAddress ip = webSocket.remoteIP(num);
-        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\r\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        sprintf(buffer, "[%u] Connected from %d.%d.%d.%d url: %s\r\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+        Serial.printf(buffer);
+        
         // Send the current LED status
         webSocket.sendTXT(0, "HELLO", 5);
-      }
-      break;
+
+        oled.clearDisplay();
+        oled.setTextXY(0,0);
+        oled.putString("Client connected");
+        oled.setTextXY(1,0);
+        oled.putString(buffer);
+    }
+    break;
     case WStype_TEXT:
-      Serial.printf("[%u] get Text: %s\r\n", num, payload);
+        Serial.printf("[%u] get Text: %s\r\n", num, payload);
 
-// motor:<leftspeed>:<rightspeed>
-      if (strncmp("motor", (const char *)payload, 5) == 0) {
-           const char delim[2] = ":";
-           char *token;
+        // motor:<leftspeed>:<rightspeed>
+        if (strncmp("motor", (const char *)payload, 5) == 0)
+        {
+            const char delim[2] = ":";
+            char *token;
 
-// expect "motor"
-           token = strtok((char *)payload, delim);
+            // expect "motor"
+            token = strtok((char *)payload, delim);
 
-// expect left speed as string
-           token = strtok(NULL, delim);
+            // expect left speed as string
+            token = strtok(NULL, delim);
 
-           motor_speed_left = atoi(token);
+            motor_speed_left = atoi(token);
 
-// expect right speed as string
-           token = strtok(NULL, delim);
+            // expect right speed as string
+            token = strtok(NULL, delim);
 
-           motor_speed_right = atoi(token);
+            motor_speed_right = atoi(token);
 
+            setMotorSpeed(motor_speed_left, motor_speed_right);
+            Serial.print("Setting motor speed ");
+            Serial.print(motor_speed_left);
+            Serial.print(' ');
+            Serial.println(motor_speed_right);
 
-           setMotorSpeed(motor_speed_left, motor_speed_right);
-           Serial.print("Setting motor speed ");
-           Serial.print(motor_speed_left);
-           Serial.print(' ');
-           Serial.println(motor_speed_right);
-       } else if (strcmp("go", (const char *)payload) == 0) {
-        robot_go();
-      }
-else if (strcmp("on", (const char *)payload) == 0) {
-        robot_on();
-      }
-      else if (strcmp("off", (const char *)payload) == 0) {
-        robot_off();
-      }
-      
-      else if (strcmp("stop", (const char *)payload) == 0) {
-        robot_stop();
-      }
-      else if (strcmp("left", (const char *)payload) == 0) {
-        robot_left();
-      }
-      else if (strcmp("right", (const char *)payload) == 0) {
-        robot_right();
-      }
-      else if (strcmp("back", (const char *)payload) == 0) {
-        robot_back();
-      }
-      else {
-        Serial.print("Unknown command: ");
-        Serial.println((const char *)payload);
-      }
-      // send data to all connected clients
-      webSocket.broadcastTXT(payload, length);
-      break;
+            /*
+            oled.setTextXY(0,0);
+            oled.putString("Motor speed");
+            oled.setTextXY(1,0);
+            oled.putString("       ");
+            oled.putNumber(motor_speed_left);
+            oled.setTextXY(2,0);
+            oled.putString("       ");
+            oled.putNumber(motor_speed_right);
+            */
+        }
+        else if (strcmp("go", (const char *)payload) == 0)
+        {
+            robot_go();
+        }
+        else if (strcmp("on", (const char *)payload) == 0)
+        {
+            robot_on();
+            oled.setTextXY(0,0);
+            oled.putString("FIRE!        ");
+        }
+        else if (strcmp("off", (const char *)payload) == 0)
+        {
+            robot_off();
+        }
+
+        else if (strcmp("stop", (const char *)payload) == 0)
+        {
+            robot_stop();
+        }
+        else if (strcmp("left", (const char *)payload) == 0)
+        {
+            robot_left();
+        }
+        else if (strcmp("right", (const char *)payload) == 0)
+        {
+            robot_right();
+        }
+        else if (strcmp("back", (const char *)payload) == 0)
+        {
+            robot_back();
+        }
+        else
+        {
+            Serial.print("Unknown command: ");
+            Serial.println((const char *)payload);
+        }
+        // send data to all connected clients
+        //webSocket.broadcastTXT(payload, length);
+        break;
     case WStype_BIN:
-      Serial.printf("[%u] get binary length: %u\r\n", num, length);
-      hexdump(payload, length);
+        Serial.printf("[%u] get binary length: %u\r\n", num, length);
+        //hexdump(payload, length);
 
-      // echo data back to browser
-      webSocket.sendBIN(num, payload, length);
-      break;
+        // echo data back to browser
+        //webSocket.sendBIN(num, payload, length);
+        break;
     default:
-      Serial.printf("Invalid WStype [%d]\r\n", type);
-      break;
-  }
+        Serial.printf("Invalid WStype [%d]\r\n", type);
+        break;
+    }
 }
-
 
 void handleRoot()
 {
-  server.send_P(200, "text/html", INDEX_HTML);
+    server.send_P(200, "text/html", INDEX_HTML);
+}
+
+void handleJoystickJs()
+{
+    server.send_P(200, "text/javascript", JOYSTICK_JS);
 }
 
 void handleNotFound()
 {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
 }
 
+
+String IPAddress2String(IPAddress address)
+{
+ return String(address[0]) + "." + 
+        String(address[1]) + "." + 
+        String(address[2]) + "." + 
+        String(address[3]);
+}
 
 void setup(void)
 {
-  // Start Serial
-  Serial.begin(115200);
+    // Start Serial
+    Serial.begin(115200);
 
-  pinMode(DIRA, OUTPUT);
-  pinMode(DIRB, OUTPUT);
-  pinMode(PWMA, OUTPUT);
-  pinMode(PWMB, OUTPUT);
-  pinMode(POWER_PIN, OUTPUT);
-  // pinMode(HEADLIGHT, OUTPUT);
+    pinMode(DIRA, OUTPUT);
+    pinMode(DIRB, OUTPUT);
+    pinMode(PWMA, OUTPUT);
+    pinMode(PWMB, OUTPUT);
+    pinMode(POWER_PIN, OUTPUT);
+    // pinMode(HEADLIGHT, OUTPUT);
+
+    analogWrite(PWMA, 0);
+    analogWrite(PWMB, 0);
+
+    digitalWrite(DIRA, HIGH);
+    digitalWrite(DIRB, LOW);
+
+    digitalWrite(POWER_PIN, LOW);
+    //  digitalWrite(HEADLIGHT, HIGH);
+
+    Serial.println();
+    Serial.println();
+    Serial.println();
+    Serial.println("HELLO");
+
+Wire.begin();  
+  oled.init();                      // Initialze SSD1306 OLED display
+  oled.clearDisplay();              // Clear screen
+  oled.setTextXY(0,0);
+    // Use full 256 char 'Code Page 437' font
 
 
-  analogWrite(PWMA, 0);
-  analogWrite(PWMB, 0);
+oled.putString("Connecting to Wi-Fi...");
+  
 
-  digitalWrite(DIRA, HIGH);
-  digitalWrite(DIRB, LOW);
+// my mobile hotspot
+    WiFiMulti.addAP("AndroidAP", "uche7888");
+    WiFiMulti.addAP(ssid, password);
 
-  digitalWrite(POWER_PIN, LOW);
-  //  digitalWrite(HEADLIGHT, HIGH);
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
+        //oled.write('.');
+        //oled.display();
+        Serial.print(".");
+        delay(100);
+    }
 
-Serial.println();
-  Serial.println();
-  Serial.println();
-  Serial.println("HELLO");
 
-  WiFiMulti.addAP("AndroidAP", "uche7888");
-  WiFiMulti.addAP(ssid, password);
 
-  while(WiFiMulti.run() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(100);
-  }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.println(WiFi.localIP());
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    oled.clearDisplay();
+    oled.setTextXY(0, 0);
+    oled.putString("Connected");
+    oled.setTextXY(1, 0);
+    oled.putString(ssid);
+    oled.setTextXY(2, 0);
+    oled.putString(IPAddress2String(WiFi.localIP()));
+    
+    if (mdns.begin("t100", WiFi.localIP()))
+    {
+        Serial.println("MDNS responder started");
+        mdns.addService("http", "tcp", 80);
+        mdns.addService("ws", "tcp", 81);
+    }
+    else
+    {
+        Serial.println("MDNS.begin failed");
+    }
+    Serial.print("Connect to http://t100.local");
+    Serial.println("");
+    Serial.println(WiFi.localIP());
 
-  if (mdns.begin("t100", WiFi.localIP())) {
-    Serial.println("MDNS responder started");
-    mdns.addService("http", "tcp", 80);
-    mdns.addService("ws", "tcp", 81);
-  }
-  else {
-    Serial.println("MDNS.begin failed");
-  }
-  Serial.print("Connect to http://t100.local");
-  Serial.println("");
-  Serial.println(WiFi.localIP());
+    server.on("/", handleRoot);
+    server.on("/virtualjoystick.js", handleJoystickJs);
+    server.onNotFound(handleNotFound);
 
-  server.on("/", handleRoot);
-  server.onNotFound(handleNotFound);
+    server.begin();
 
-  server.begin();
-
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
 }
 
-void loop() {
-  webSocket.loop();
-  server.handleClient();
+void loop()
+{
+    webSocket.loop();
+    server.handleClient();
 
-/*  
+    /*  
   if (Serial.available() > 0) {
     // read the incoming byte:
     char incomingByte = Serial.read();
@@ -851,108 +962,115 @@ void loop() {
   }
   */
 
+
 }
 
 void setMotorSpeed(int leftSpeed, int rightSpeed)
 {
-const int minSpeedTreshhold = 10;
-const int maxSpeedTreshhold = 100;
+    const int minSpeedTreshhold = 10;
+    const int maxSpeedTreshhold = 100;
 
-const int minPwmSpeed = PWMRANGE / 4;
-const int maxPwmSpeed = PWMRANGE;
-  
-if (abs(leftSpeed) < minSpeedTreshhold) {
-  leftSpeed = 0;
-}
+    const int minPwmSpeed = PWMRANGE / 4;
+    const int maxPwmSpeed = PWMRANGE;
 
-if (abs(rightSpeed) < minSpeedTreshhold) {
-  rightSpeed = 0;
-}
+    if (abs(leftSpeed) < minSpeedTreshhold)
+    {
+        leftSpeed = 0;
+    }
 
+    if (abs(rightSpeed) < minSpeedTreshhold)
+    {
+        rightSpeed = 0;
+    }
 
-  
-  digitalWrite(DIRA, leftSpeed < 0 ? HIGH : LOW);
-  digitalWrite(DIRB, rightSpeed > 0 ? HIGH : LOW);
+    digitalWrite(DIRA, leftSpeed < 0 ? HIGH : LOW);
+    digitalWrite(DIRB, rightSpeed > 0 ? HIGH : LOW);
 
-if (leftSpeed) {
-  analogWrite(PWMA, map(abs(leftSpeed), 0, maxSpeedTreshhold, minPwmSpeed, maxPwmSpeed));
-} else {
-  analogWrite(PWMA, 0);
-}
+    if (leftSpeed)
+    {
+        analogWrite(PWMA, map(abs(leftSpeed), 0, maxSpeedTreshhold, minPwmSpeed, maxPwmSpeed));
+    }
+    else
+    {
+        analogWrite(PWMA, 0);
+    }
 
-  if (rightSpeed) {
-    analogWrite(PWMB, map(abs(rightSpeed), 0, maxSpeedTreshhold, minPwmSpeed, maxPwmSpeed));
-  } else {
-    analogWrite(PWMB, 0);
-  }
-  
+    if (rightSpeed)
+    {
+        analogWrite(PWMB, map(abs(rightSpeed), 0, maxSpeedTreshhold, minPwmSpeed, maxPwmSpeed));
+    }
+    else
+    {
+        analogWrite(PWMB, 0);
+    }
 }
 
 void motor()
 {
-  digitalWrite(DIRA, dirA);
-  digitalWrite(DIRB, dirB);
-  analogWrite(PWMA, xspeed);
-  analogWrite(PWMB, xspeed);
+    digitalWrite(DIRA, dirA);
+    digitalWrite(DIRB, dirB);
+    analogWrite(PWMA, xspeed);
+    analogWrite(PWMB, xspeed);
 }
 
+int robot_left()
+{
+    dirA = LOW;
+    dirB = LOW;
 
+    motor();
 
-int robot_left() {
-  dirA = LOW;
-  dirB = LOW;
-
-
-  motor();
-
-  return 1;
+    return 1;
 }
 
-int robot_right() {
-  dirA = HIGH;
-  dirB = HIGH;
+int robot_right()
+{
+    dirA = HIGH;
+    dirB = HIGH;
 
-
-  motor();
-  return 1;
+    motor();
+    return 1;
 }
 
-int robot_go() {
-  dirA = HIGH;
-  dirB = LOW;
+int robot_go()
+{
+    dirA = HIGH;
+    dirB = LOW;
 
-  if (xspeed == 0)  {
-    xspeed = PWMRANGE;
-  }
+    if (xspeed == 0)
+    {
+        xspeed = PWMRANGE;
+    }
 
-  motor();
-  return 1;
+    motor();
+    return 1;
 }
 
+int robot_back()
+{
+    dirA = LOW;
+    dirB = HIGH;
 
-int robot_back() {
-  dirA = LOW;
-  dirB = HIGH;
-
-  motor();
-  return 1;
+    motor();
+    return 1;
 }
 
-int robot_stop() {
-  dirA = HIGH;
-  dirB = LOW;
-  xspeed = 0;
-  motor();
+int robot_stop()
+{
+    dirA = HIGH;
+    dirB = LOW;
+    xspeed = 0;
+    motor();
 
-  return 1;
+    return 1;
 }
 
 void robot_on()
 {
-digitalWrite(POWER_PIN, HIGH);
+    digitalWrite(POWER_PIN, HIGH);
 }
 
 void robot_off()
 {
-digitalWrite(POWER_PIN, LOW);
+    digitalWrite(POWER_PIN, LOW);
 }
